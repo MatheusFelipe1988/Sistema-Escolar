97592ca90dd1e24eb1f19a20a8393e81b3cbcb9d
MFel33 <104394609+MatheusFelipe1988@users.noreply.github.com>
2023-12-17 14:11:43 -0400

    Package
    classes utilizadas

---
+++ escolar/src/main/java/com/system/escolar/SistemaEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.system.escolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEscolarApplication.class, args);
+	}
+
+}
---
+++ escolar/src/test/java/com/system/escolar/SistemaEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.escolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
